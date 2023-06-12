4409084721643c62499e58a53f3b4d9d637f3e3e
Gabrijel <g.gordic@outlook.com>
2023-06-12 22:30:05 +0200

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -3,12 +3,39 @@ import customtkinter as ctk
 
 
 
-
-root = ctk.CTl
-
+def main():
+    root = ctk.CTk()
+    
+    def sign_up_window_f():
+        
+        sign_up_window = Window(root, "PyFlora Posude", "800x600" )
+        
+        
+        pass
+    
+    
+    sign_up_window_f()
+    return None
 
 class Window():
     
-    def __init__(self,title,geometry,message):
+    def __init__(self,root,title,geometry):
+        self.root = root
+        self.root.title(title)
+        self.root.geometry(geometry)
+        self.root.mainloop()
+        
         pass
     pass
+
+class Button():
+    
+    def __init__(self,root,message, geometry) -> None:
+        self.root = root
+        self.root.geometry(geometry)
+        ctk.Label(self.root, text=message).pack()
+        
+    
+        pass
+
+main()
