0a0ea04ac8349e8d12a581e987e4a93b92177b45
Gabrijel <g.gordic@outlook.com>
2023-06-13 16:00:04 +0200

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -1,41 +1,13 @@
-import tkinter as tk
+import tkinter as tk 
 import customtkinter as ctk 
 
+window = ctk.CTk()
+window.geometry("600x600+525+40")
+window.title('PyFlora Posude')
+window.iconbitmap('path ico slike')
 
 
-def main():
-    root = ctk.CTk()
-    
-    def sign_up_window_f():
-        
-        sign_up_window = Window(root, "PyFlora Posude", "800x600" )
-        
-        
-        pass
-    
-    
-    sign_up_window_f()
-    return None
 
-class Window():
-    
-    def __init__(self,root,title,geometry):
-        self.root = root
-        self.root.title(title)
-        self.root.geometry(geometry)
-        self.root.mainloop()
-        
-        pass
-    pass
 
-class Button():
-    
-    def __init__(self,root,message, geometry) -> None:
-        self.root = root
-        self.root.geometry(geometry)
-        ctk.Label(self.root, text=message).pack()
-        
-    
-        pass
 
-main()
+window.mainloop()
